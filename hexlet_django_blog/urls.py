592ee9419966d961d10adc1046cdb144f374ce7b
alex <leksfix@gmail.com>
2025-07-27 21:23:13 +0200

    Step 7

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.IndexView.as_view()),
     path("articles/", include("hexlet_django_blog.article.urls")),
-    path("about/", views.about),
+    path("about/", views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-
+'''
 def index(request):
     return render(
         request,
@@ -9,6 +10,16 @@ def index(request):
             "who": "World",
         },
     )
+'''
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["who"] = "World (class)"
+        return context
+
 
 def about(request):
     return render(request, "about.html")
